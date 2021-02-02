42714a485a8396f870ea5c67058faaa4432fa6f1
lbvd <lbvd@protonmail.com>
2021-02-02 15:45:26 +0100

    Delete _site directory

--- _site/js/fade.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#maincontent").fadeToggle(3000);
-    alert("Klick den INFO Button um die Sektion wieder zu entfernen");
-  });
-});
